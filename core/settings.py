341f469141c6abc97eaed3f407ccdd846dd90d6f
Tanit44 <47166064+Tanit44@users.noreply.github.com>
2022-01-31 18:58:50 +0700

    update

--- core/settings.py
+++ core/settings.py
@@ -131,9 +131,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'app/static') # follow static floder lacation
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static') # follow static floder lacation
+]
 
 # django_heroku.settings(locals())
 
